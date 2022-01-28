d8643997060cb89746ee915aa314b2b6be31d278
Alexr360 <alexr000360@gmail.com>
2022-01-27 18:09:58 -0700

    - Added more detail to shooter subsystem processes

--- src/main/java/frc/swerverobot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/swerverobot/subsystems/ShooterSubsystem.java
@@ -33,32 +33,43 @@ Notes
 
  - Processes
     - Shooting Process #1 (No Aim Assist & 2 Balls)
-        - Driver Preses Shoot Button
-        - Front 2 wheels spin up to speed (0.5s)
-        - Release first ball
-        - If there is a second ball
-            - Wait for wheels to regain lost speed (0.5s)
-            - Release Second Ball
+        - Driver Preses Shoot Button or Activated by code (Start Function)
+        - Front 2 wheels spin up to speed
+        - Wait ~0.5 Seconds
+        - Turn Storage Motor to release first Ball
+        - Wait ~0.1 Seconds
+        - Stop Storsge Motor
+        - Use Color Sensor to detect if there is still a ball in the first slot
+        - If there is a ball
+            - Wait for wheels to regain lost speed ~0.5s
+            - Turn Storage Motor to release second Ball (~0.1s)
+            - Wait for ball to exit shooter ~0.1s
+        - Stop both intake motors
         - End
         
     - Shooting Process #2 (Aim Assist & 2 Balls)
-        - Driver Preses aim button for either high or low (Maybe a button combination for the low goal)
-        - Machiene vision to find the reflective tape at the top of the hub
-        - Robot rotates shooter to face hub (Either rotates entire robot or just the turret)
-        - Uses Distance sensor to detect distance to hub and change verticle offset (If Verticle Availible if not adjust power as much as possible)
-        - Driver should then press manual shoot button (Process #1)
+        - Driver should position robot roughly aming at the hub before running program
+        - Driver Preses Aim Button or Activated by code high or low (Maybe a button combination for the low goal) (Start Function)
+        - Machiene vision to find the reflective tape on high goal
+        - Robot rotates shooter to face hub
+        - When facing hub
+        - Use Distance sensor to detect distance to hub 
+        - If high goal
+            - Ajust power of top and bottom motor to get required distance (Using high goal Calibration)
+            - If distance is not possible
+                - Notify Driver (Maybe a beep or just something on the HUD)
+        - else
+            - Ajust power of top and bottom motor to get required distance (Using low goal Calibration)
+            - If distance is not possible
+                - Notify Driver (Maybe a beep or just something on the HUD)
         - End
         
     - Shooting Process #3 (Auto Aim/Auto Shoot)
-        - Driver Preses Shoot Button for either high or low (Maybe a button combination for the low goal)
-        - Start front 2 wheels spinning(Done at same time as aiming)
-        - Machiene vision to find the reflective tape at the top of the hub
-        - Robot rotates shooter to face hub (Either rotates entire robot or just the turret)
-        - Uses Distance sensor to detect distance to hub and change verticle offset (If Verticle Availible if not adjust power as much as possible)
-        - Release first ball
-        - If there is a second ball
-            - Wait for wheels to regain lost speed (0.5s)
-            - Release Second Ball
+        - Driver should position robot roughly aming at the hub before running program
+        - Driver Preses Auto Shoot Button for either high or low (Maybe a button combination for the low goal)
+        - Run Shooting Process #1
+        - When #1 finished
+        - Run Shooting Process #2 (Input high low value from driver to process 2)
         - End
 
 */
@@ -115,10 +126,10 @@ public class ShooterSubsystem extends SubsystemBase{
 
         int ballCount = 0;//Starts the count of balls at 0
 
-        if (detectedColor.red > 1/*Red Min Value*/ && detectedColor.red < 5/*Red Max Value*/ || detectedColor.blue > 1/*Blue Min Value*/ && detectedColor.red < 5/*Blue Max Value*/) {
+        if (detectedColor.red > 1/*Red Min Value*/ && detectedColor.red < 5/*Red Max Value*/ || detectedColor.blue > 1/*Blue Min Value*/ && detectedColor.blue < 5/*Blue Max Value*/) {
             //1 Ball Found Look for next one
             ballCount = 1;
-            if (detectedColor.red > 1/*Red Min Value*/ && detectedColor.red < 5/*Red Max Value*/ || detectedColor.blue > 1/*Blue Min Value*/ && detectedColor.red < 5/*Blue Max Value*/) {
+            if (detectedColor.red > 1/*Red Min Value*/ && detectedColor.red < 5/*Red Max Value*/ || detectedColor.blue > 1/*Blue Min Value*/ && detectedColor.blue < 5/*Blue Max Value*/) {
                 //1 Ball Found Look for next one
                 ballCount = 2;
             } 
