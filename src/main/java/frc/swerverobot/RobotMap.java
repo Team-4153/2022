5efa82b3ca92c895ef4344479a32838e0e2f1b84
Alexr360 <alexr000360@gmail.com>
2022-02-08 18:52:58 -0700

    Labeled Items in Robot Map for their Subsystems

--- src/main/java/frc/swerverobot/RobotMap.java
+++ src/main/java/frc/swerverobot/RobotMap.java
@@ -11,24 +11,24 @@ import org.frcteam2910.common.robot.input.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Button;
 
 public class RobotMap {
-// define all ports and constants
+//Define all ports and constants of a component, put in correct catagory, label subsystem(s) used
 
 //Sensors
     public static final ADIS16470_IMU imu = new ADIS16470_IMU(); //Gyro and acceleration sensor
-    public static final I2C.Port i2cPort = I2C.Port.kOnboard; //Color Sensor (Only one I2C port on the robot)
+    public static final I2C.Port i2cPort = I2C.Port.kOnboard; //[Shooter Subsystem] Color Sensor (Only one I2C port on the robot)
 //Motors
-    public static final int TopMotorPort = 0; // The Number is the RIO PWM port
-    public static final int BottomMotorPort = 1; // The Number is the RIO PWM port
-    public static final int StorageMotorPort = 2; // The Number is the RIO PWM port
-    public static final int Intake_Motor_PWM = 3; // PWM for intake motor
-    public static final int CLIMBER_MOTOR = 9;
+    public static final int TopMotorPort = 0; //[Shooter Subsystem] The Number is the RIO PWM port 
+    public static final int BottomMotorPort = 1; //[Shooter Subsystem] The Number is the RIO PWM port
+    public static final int StorageMotorPort = 2; //[Shooter Subsystem] The Number is the RIO PWM port
+    public static final int Intake_Motor_PWM = 3; //[Intake Subsystem] PWM for intake motor
+    public static final int CLIMBER_MOTOR = 9; //[Climber Subsystem] The Number is the RIO PWM port
 //Controls
-    public static final Controller Driver_controller = new XboxController(0);
-    public static final Button Intake_Extension = Driver_controller.getLeftBumperButton();
-    public static final Button Intake_Roller = Driver_controller.getRightBumperButton();
+    public static final Controller Driver_controller = new XboxController(0);//[Controller]
+    public static final Button Intake_Extension = Driver_controller.getLeftBumperButton();//[Intake Subsystem]
+    public static final Button Intake_Roller = Driver_controller.getRightBumperButton();//[Intake Subsystem]
 //Pneumatics
-    public static final int INTAKE_SOLa = 1;
-    public static final int INTAKE_SOLb = 2;
+    public static final int INTAKE_SOLa = 1;//[Intake Subsystem]
+    public static final int INTAKE_SOLb = 2;//[Intake Subsystem]
 
 //Drive Train
 // EVEN numbers are angle motors, ODD numbers are drive motors
@@ -52,7 +52,7 @@ public class RobotMap {
     public static final double DRIVETRAIN_BACK_LEFT_MODULE_ANGLE_OFFSET = -Math.toRadians(357.2);
     public static final int DRIVETRAIN_BACK_LEFT_MODULE_DRIVE_MOTOR = 7;
 
-    //Climber Stuff
+//Climber Stuff TODO: fix
     public static final int CLIMBER_PISTON = 1;
     public static final int CLIMBER_SWITCH = 0;
     public static final int WINCH_PISTON = 2;
