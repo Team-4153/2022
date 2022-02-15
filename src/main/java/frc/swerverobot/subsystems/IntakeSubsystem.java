ddfd86ecdc447edb173c11af04816822d33b2031
Team 4153 <pylebas@gmail.com>
2022-02-14 19:46:53 -0700

    added color sensor functionality to intake

--- src/main/java/frc/swerverobot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/swerverobot/subsystems/IntakeSubsystem.java
@@ -1,40 +1,110 @@
 package frc.swerverobot.subsystems;
 
+//
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// Pneumatics
+import static frc.swerverobot.RobotMap.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+//Pneumatics
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 //Motors
 import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 
-//Robot Map
-import static frc.swerverobot.RobotMap.*;
+//Sensors
+import edu.wpi.first.wpilibj.util.Color;
+import com.revrobotics.ColorSensorV3;
+import edu.wpi.first.wpilibj.DigitalInput;
 
-//Controller
-// import org.frcteam2910.common.robot.input.Axis;
-// import org.frcteam2910.common.robot.input.Controller;
-// import org.frcteam2910.common.robot.input.XboxController;
-// import frc.swerverobot.XboxTrigger;
+
 
 public class IntakeSubsystem extends SubsystemBase {
   /** Creates a new IntakeSubsystem. */
   public IntakeSubsystem() {}
 
-  //public final Compressor phCompressor = new Compressor(1, PneumaticsModuleType.REVPH);
   private DoubleSolenoid Intake_Sol = new DoubleSolenoid(PH_CAN_ID, PneumaticsModuleType.REVPH, INTAKE_SOLa, INTAKE_SOLb);
   private PWMVictorSPX Intake_Motor = new PWMVictorSPX(Intake_Motor_PWM);
 
+  boolean ballStuck = false; //Is true if there is a ball in the second position(Photoeye) but not the first(Color Sensor)
+
+  //Color Sensor
+  public final ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort); //The Number is the I2C port from the RobotMap.java
+    
+  //Photo Eye (Digital Input|Boolean)
+  DigitalInput photoEye = new DigitalInput(PhotoEyePort);
+
   // private boolean triggerDone = false;
 
+  //      ----Ball Count & Color Functions----
+  public int ballCount() {
+    //Starts the count of balls at 0
+    int ballCount = 0;
+
+    //Look for first ball with color
+    if (ball1color() != "none") {
+        ballStuck = false;
+        
+        //1 Ball Found
+        ballCount = 1;
+
+        //Look for second ball with photo eye
+        if (photoEye.get()) {
+            //2 Balls found
+            ballCount = 2;
+        }
+    }
+    else {
+        //Check if there is a ball in second position but not first
+        if (photoEye.get()) {
+            //A ball is in the second position but not the first
+            ballCount = 1;
+            ballStuck = true;
+        }
+    }
+
+    //No else statment because value is initalized at 0
+    SmartDashboard.putNumber("Ball Count", ballCount);
+    SmartDashboard.putBoolean("Ball Stuck", ballStuck);
+    return ballCount;
+}
+  public String ball1color() {
+    //Detected Color & Proximity from Color Sensor 1
+    Color detectedColor = colorSensor.getColor();
+    int proximity = colorSensor.getProximity();
+
+    String ball1Color = "none";
+
+    //Check ball color
+    if (proximity > 125) {//125 is 1 inch and half a ball away from the color sensor
+        if (detectedColor.red > detectedColor.blue) {
+            //The 1st ball is Red
+            //set variable to be returned to Red
+            ball1Color = "Red";
+        }
+        else {
+            //The 1st ball is Blue
+            //set variable to be returned to Blue
+            ball1Color = "Blue";
+        }
+    }
+    
+    //No else statment because value is initalized at "none"
+    SmartDashboard.putString("Ball 1 Color", ball1Color);
+    return ball1Color;
+}
+
+
   @Override
-  public void periodic() {
+  public void periodic() 
+  {
 
   }
 
   @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+  public void simulationPeriodic() 
+  {
+
   }
 
   public void Motor_init ()
@@ -42,26 +112,19 @@ public class IntakeSubsystem extends SubsystemBase {
     Intake_Motor.setSafetyEnabled(false);
   }
 
-  // public void Motor_Start ()
-  // {
-  //   Intake_Motor.set(.5);
-  // }
-
-  // public void Motor_Stop ()
-  // {
-  //   Intake_Motor.set(0);
-  // }
 
   public void Sol_init () 
   {
-    // This method sets the solonoid to a position on bootup
     Intake_Sol.set(DoubleSolenoid.Value.kReverse);
   }
 
   public void Extend () 
   {
-    Intake_Sol.set(DoubleSolenoid.Value.kForward);
-    Intake_Motor.set(0.5);
+    int ballcount = ballCount();
+    if (ballcount == 0) {
+      Intake_Sol.set(DoubleSolenoid.Value.kForward);
+      Intake_Motor.set(0.5);
+    }
   }
 
   public void Retract () 
