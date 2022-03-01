1fe4c2dca77214b180d81a41fab52a46a9dc93db
Alexr360 <70153720+Alexr360@users.noreply.github.com>
2022-02-28 21:59:00 -0700

    Migrated Notes from Old Shooter Subsystem

--- src/main/java/frc/swerverobot/subsystems/ShooterSubsystem2.java
+++ src/main/java/frc/swerverobot/subsystems/ShooterSubsystem2.java
@@ -131,3 +131,107 @@ public class ShooterSubsystem2 extends SubsystemBase {
         pushDashboardVars();
     }
 }
+
+/*      ----Notes----
+    - Motors
+        - Prototype used 2 launch motors and 1 feed motor
+        - Balls are Stored with feed Motor
+    - Distance to target with machine vision (Rasberry pie with network cable?|TODO:Figure out network tables/Rasberry pie)
+    - TODO: Redo Timing in ShootCommand.java
+
+    - Notes from Prototype Testing
+        - With Current Motors and angle of ~30 degrees power of 0.6T 0.6B works well to get it into the goal from distance
+        - From Close up adding backspin helps
+*/
+
+/*      ----Electronics---- 
+    - Motors (3|Functional)
+        - 2 for shooter
+        - 1 for feed
+    - Motor Controllers (3|Functional)
+        - 1 for each motor in the shooter
+        - Spark Motro Controllers
+    - Color Sensor (1|Functional)
+        - Detect first ball color and count
+    - Photo Eye (1|Functional)
+        - Detect if there is a second ball
+        - Detect if there is only a second ball and not a first one
+    - Machine Vision
+        - Camera (1?)
+        - Rasberry Pie (1)
+            - Figure out network tables
+        - Needs to be able to Detect high goal & Distance to hub
+            - Detect distance to hub for shooting processs #2 and #3
+            - If distance to hub is not viable we need to find some way of reliably detecting distance to hub
+                - Things other than machine vision might need to be aimed upwards because the bottom section is not uniform maybe aim for middle of the lower hoop from edge of the field?
+    - PID Controller (Being Added?)
+        - Keep flywheels at constant speed
+        - Might not be needed
+*/
+
+/*      ----Driver Interaction----          (TODO: Update Controlls)
+    All controls should be on Noahs controller
+    - shootingProcess1(X:Needs Testing) - Shoots balls this will use values from auto aim to shoot, The driver can also manually change these values with 
+    - shootingProcess2(Not Needed) - Auto aims the robot but doesent shoot, the boolean is for aiming for the high or low goal (true = High || false = Low)
+    - shootingProcess3(Right Trigger for High Goa0l:Needs Testing | Left Trigger for Low Goal:Needs Testing) - Auto aims the robot and shoots, the boolean is for aiming for the high or low goal (true = High || false = Low)
+    - manualShooterDistanceIncrease(Y:Functional) - Increases the power to both shooter motors by 5%, doesent shoot balls
+    - manualShooterDistanceDecrease(A:Functional) - Decrease the power to both shooter motors by 5%, doesent shoot balls
+    - dropBall(B:Needs Testing) - Drops the first ball in the system
+*/
+
+/*      ----Processes (Current|Shooter Subsystem #2)----
+
+*/
+
+/*      ----Processes (Outdated|Shooter Subsystem #1)----
+    - Shooting Process #1 (No Aim Assist & 2 Balls | Needs Testing)
+        - If Process isnt already running
+        - Driver Preses Shoot Button or Activated by code (Start Function)
+        - Spin bottom & top Motors to variables (This value can be changed by the driver manually or buy the aim program)
+        - Wait ~0.5 Seconds
+        - Activate Feed Motor to release first Ball (20% power worked in testing)
+        - Wait ~0.1 Seconds
+        - Stop Feed Motor
+        - Use Color Sensor to detect if there is still a ball in the first slot
+        - If there is a ball
+            - Wait for wheels to regain lost speed ~0.5s
+            - Turn Feed Motor to release second Ball
+            - Wait for ball to exit shooter ~0.1s
+        - Stop both intake motors
+        - End
+        
+    - Shooting Process #2 (Aim Assist & 2 Balls | Needs Testing)
+        - If Process isnt already running
+        - Driver should position robot roughly aming at the hub before running program
+        - Driver Preses Aim Button or Activated by code high or low (Maybe a button combination for the low goal) (Start Function)
+        - Machiene vision to find the reflective tape on high goal
+        - Rotate Robot to face hub
+        - Wait Until facing hub
+        - Use Distance sensor or Machine Vision to detect distance to hub 
+        - If high goal
+            - Ajust power of top and bottom motor to get required distance (Using high goal Calibration)
+            - If distance is not possible
+                - Notify Driver (Maybe a beep or just something on the HUD)
+        - else
+            - Ajust power of top and bottom motor to get required distance (Using low goal Calibration)
+            - If distance is not possible
+                - Notify Driver (Maybe a beep or just something on the HUD)
+        - End
+        
+    - Shooting Process #3 (Auto Aim/Auto Shoot | Needs Testing)
+        - If Process isnt already running
+        - Driver should position robot roughly aming at the hub before running program
+        - Driver Preses Auto Shoot Button for either high or low (Maybe a button combination for the low goal)
+        - Run Shooting Process #2 (Input High Low Goal)
+        - Wait until #2 finished
+        - Run Shooting Process #1 
+        - End
+    - Drop Balls
+        - If Process isnt already running
+        - Driver presses button
+        - Gently spin shoot motors
+        - Push forward first ball
+        - Wait 0.2 Seconds
+        - Stop All Motors
+        - End
+*/
