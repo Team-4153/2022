52a7b955a28a5fdaffc65da8b12a618cd4aa3d8a
Alexr360 <70153720+Alexr360@users.noreply.github.com>
2022-02-28 21:58:41 -0700

    Organized ShootCommand.java

--- src/main/java/frc/swerverobot/commands/shooter/ShootCommand.java
+++ src/main/java/frc/swerverobot/commands/shooter/ShootCommand.java
@@ -1,52 +1,55 @@
 package frc.swerverobot.commands.shooter;
 
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.swerverobot.subsystems.ShooterSubsystem2;
 
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+
+//              -----Shoot Command-----
+/*
+ - Count balls in shooter
+ - If there are 2 balls, shoot
+*/
 public class ShootCommand extends SequentialCommandGroup{
-    private double time = 0.01; // subject to change
     private final ShooterSubsystem2 shooter;
     private double topSpeed;
     private double botSpeed;
+
     private double feedSpeed = -0.5;
+
     private double waitforSpool = 1.5;
     private double waitforShoot1ball = 1.5;
     private double waitforShoot2balls = 0.75;
+
+    double waitForShoot = waitforShoot2balls; //This variable is changed for the number of balls in the shooter
 
     public ShootCommand(ShooterSubsystem2 shooter, double topSpeed, double botSpeed) {
         this.shooter = shooter;
         this.topSpeed = topSpeed;
         this.botSpeed = botSpeed;
 
-        if (shooter.getBallCount() < 1) {
-            //For 2 Balls
-            addCommands(
-                new RunShootMotors(shooter, topSpeed,  botSpeed),
-    
-                new WaitCommand(waitforSpool),
-                new RunFeedMotors(shooter, feedSpeed),
-    
-                new WaitCommand(waitforShoot2balls),
-                new RunFeedMotors(shooter, 0),
-                new RunShootMotors(shooter, 0,  0)
-            );
+        //Changes the wait time for the number of balls in the shooter
+        if (waitForShoot < 1) {
+            //if there are 2 balls use the 2 ball wait time
+            waitForShoot = waitforShoot2balls;
         }
         else {
-            //For 1 Ball
-            addCommands(
-                new RunShootMotors(shooter, topSpeed,  botSpeed),
-
-                new WaitCommand(waitforSpool),
-                new RunFeedMotors(shooter, feedSpeed),
-
-                new WaitCommand(waitforShoot1ball),
-                new RunFeedMotors(shooter, 0),
-                new RunShootMotors(shooter, 0,  0)
-                
-            );
+            //if there is 1 ball use the 1 ball wait time
+            waitForShoot = waitforShoot1ball;
         }
 
+        //This shoots all balls in the shooter
+        addCommands(
+            new RunShootMotors(shooter, topSpeed,  botSpeed), //Spool up shooter motors
+
+            new WaitCommand(waitforSpool), //Wait for spool up of motors
+            new RunFeedMotors(shooter, feedSpeed), //Feed both balls into shooter
+
+            new WaitCommand(waitForShoot), //Wait for 2 balls to exit shooter
+            new RunFeedMotors(shooter, 0), //Stop feed motors
+            new RunShootMotors(shooter, 0,  0) //Stop shooter motors
+        );
     }
 
 }
