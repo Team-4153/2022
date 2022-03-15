0ea6f6516987370760c008035a316bc96ef27e92
Team 4153 <pylebas@gmail.com>
2022-03-14 18:04:44 -0600

    Removed Color Sensor from Shooter Subsystem

--- src/main/java/frc/swerverobot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/swerverobot/subsystems/LEDSubsystem.java
@@ -5,7 +5,6 @@
 package frc.swerverobot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import static frc.swerverobot.RobotMap.*;
