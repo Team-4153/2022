42af4b58eb4edb04e0be70f2b75898063872a162
Team 4153 <pylebas@gmail.com>
2022-03-15 17:37:58 -0600

    Updated FeedCommand & Intake Sequence to use new Photo Eye

--- src/main/java/frc/swerverobot/commands/intake/IntakeSequence.java
+++ src/main/java/frc/swerverobot/commands/intake/IntakeSequence.java
@@ -1,7 +1,6 @@
 package frc.swerverobot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-//import frc.swerverobot.subsystems.ClimberSubsystem;
 import frc.swerverobot.subsystems.IntakeSubsystem;
 import frc.swerverobot.subsystems.ShooterSubsystem2;
 
@@ -9,7 +8,7 @@ public class IntakeSequence extends SequentialCommandGroup{
     public IntakeSequence(IntakeSubsystem intake, ShooterSubsystem2 shooter) {
         addCommands(
             new IntakeCommand(intake, false),
-            new FeedCommand(shooter).withTimeout(0.3)
+            new FeedCommand(shooter)
         );
     }
 
