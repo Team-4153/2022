fa3e1e80381cb00af9240a155e5bcb2cc543d752
Alexr360 <alexr000360@gmail.com>
2022-03-24 19:10:31 -0600

    Updated LED Count

--- src/main/java/frc/swerverobot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/swerverobot/subsystems/LEDSubsystem.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import static frc.swerverobot.RobotMap.*;
 
 public class LEDSubsystem extends SubsystemBase {
-  private int lengthstrand1 = 33;
+  private int lengthstrand1 = 29;
   private int lengthstrand2 = 34;
   private int lengthstrand3 = 80;
 
