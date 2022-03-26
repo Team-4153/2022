22c891150d75c5c8b78e977c504666690f481446
Alexr360 <alexr000360@gmail.com>
2022-03-26 14:17:29 -0600

    Added Mode to SmartDashboard

--- src/main/java/frc/swerverobot/Robot.java
+++ src/main/java/frc/swerverobot/Robot.java
@@ -1,6 +1,7 @@
 package frc.swerverobot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -33,6 +34,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void autonomousInit() {
+      SmartDashboard.putString("Mode", "Auto");
       autonomousCommand = container.getAutonomousCommand();
   
       // schedule the autonomous command (example)
@@ -46,9 +48,10 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
-        if (autonomousCommand != null) {
-            autonomousCommand.cancel();
-        }
+      SmartDashboard.putString("Mode", "Tele");
+      if (autonomousCommand != null) {
+          autonomousCommand.cancel();
+      }
     }
 
     @Override
