7ee17fd72e9206925952123148bfd445a7f9c368
Alexr360 <alexr000360@gmail.com>
2022-03-30 16:31:26 -0600

    Fixed Test Command

--- src/main/java/frc/swerverobot/commands/auto/TestCommand.java
+++ src/main/java/frc/swerverobot/commands/auto/TestCommand.java
@@ -25,26 +25,27 @@ import frc.swerverobot.subsystems.ShooterSubsystem2;
 public class TestCommand extends SequentialCommandGroup{
     public TestCommand(DrivetrainSubsystem drivetrain, ShooterSubsystem2 shooter, IntakeSubsystem intake, ClimberSubsystem climb) {
         addCommands(
-            new DriveCommand(drivetrain, () -> 0.5, () -> 0, () -> 0, () -> 0, () -> 0).withTimeout(2),
-            new DriveCommand(drivetrain, () -> 0, () -> 0.5, () -> 0, () -> 0, () -> 0).withTimeout(2),
-            new DriveCommand(drivetrain, () -> 0, () -> 0, () -> 0, () -> 0, () -> 0).withTimeout(0.2),
+            new DriveCommand(drivetrain, () -> 0.5, () -> 0, () -> 0, () -> 0, () -> 0).withTimeout(1),
+            new DriveCommand(drivetrain, () -> 0, () -> 0.5, () -> 0, () -> 0, () -> 0).withTimeout(1),
+            new DriveCommand(drivetrain, () -> 0, () -> 0, () -> 0, () -> 0, () -> 0).withTimeout(0.5),
 
-            new IntakeCommand(intake, false).withTimeout(2),
+            new IntakeCommand(intake, false).withTimeout(1),
             new IntakeCommand(intake, true).withTimeout(1),
 
             new ManualShoot(shooter, -0.5, 0.5, -0.5),
             new WaitCommand(0.5),
 
-            new ArmPositionCommand(climb, States.UNLOCKED),
-            new WaitCommand(1),
             new ArmPositionCommand(climb, States.LOCKED),
-
-            new UnspoolCommand(climb).withTimeout(1.5),
-            new SpoolCommand(climb).withTimeout(1.5),
-
-            new StaticHookCommand(climb, States.LOCKED),
+            new WaitCommand(1),
+            new ArmPositionCommand(climb, States.UNLOCKED),
             new WaitCommand(0.5),
-            new StaticHookCommand(climb, States.UNLOCKED)
+
+            new UnspoolCommand(climb).withTimeout(0.3),
+            new SpoolCommand(climb).withTimeout(0.3),
+
+            new StaticHookCommand(climb, States.UNLOCKED),
+            new WaitCommand(1),
+            new StaticHookCommand(climb, States.LOCKED)
         );
     }
 
