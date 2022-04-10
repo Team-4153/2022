5d4c89e197f30e357094fa82be95921a694c4e57
Alexr360 <alexr000360@gmail.com>
2022-04-10 17:22:55 -0500

    Updated High Two Ball Auto to use Auto Aim

--- src/main/java/frc/swerverobot/commands/auto/competition/HighTwoBall.java
+++ src/main/java/frc/swerverobot/commands/auto/competition/HighTwoBall.java
@@ -6,6 +6,7 @@ import frc.swerverobot.commands.LED.SetT;
 import frc.swerverobot.commands.drive.DriveCommand;
 import frc.swerverobot.commands.drive.DriveWithSetRotationCommand;
 import frc.swerverobot.commands.intake.IntakeCommand;
+import frc.swerverobot.commands.shooter.AutoAim;
 import frc.swerverobot.commands.shooter.ManualShoot;
 import frc.swerverobot.commands.shooter.ShootCommand;
 import frc.swerverobot.subsystems.DrivetrainSubsystem;
@@ -34,14 +35,21 @@ public class HighTwoBall extends SequentialCommandGroup{
         addRequirements(drivetrain);
 
         addCommands(
+            // new SetHighGoalAuto(),                                                                          //Set the LED's to High goal colors
+            // new IntakeCommand(intake, false).withTimeout(0.1),                                              //Extend Intake
+            // new DriveCommand(drivetrain, () -> -0.5, () -> 0, () -> 0, () -> 0, () -> 0).withTimeout(1.0),  //Drive to second ball
+            // new DriveCommand(drivetrain, () -> 0, () -> 0, () -> 0, () -> 0, () -> 0).withTimeout(1.5),     //Stop Driving
+            // new IntakeCommand(intake, true).withTimeout(0.1),                                               //Retract Intake
+            // new ManualShoot(shooter, -0.4, 1, -0.6),                                                        //Shoot balls into high goal
+            // new DriveCommand(drivetrain, () -> -0.5, () -> 0, () -> 0, () -> 0, () -> 0).withTimeout(0.2),  //Drive backwards a little more to make sure it is out of tape
+            // new DriveCommand(drivetrain, () -> 0, () -> 0, () -> 0, () -> 0, () -> 0).withTimeout(1),       //Stop Driving backwards
+            // new SetT()                                                                                      //Change Auto LED's to tele Mode
             new SetHighGoalAuto(),                                                                          //Set the LED's to High goal colors
-            new IntakeCommand(intake, false).withTimeout(0.1),                                              //Extend Intake
-            new DriveCommand(drivetrain, () -> -0.5, () -> 0, () -> 0, () -> 0, () -> 0).withTimeout(1.0),  //Drive to second ball
+            // new IntakeCommand(intake, false).withTimeout(0.1),                                              //Extend Intake
+            new DriveCommand(drivetrain, () -> -0.5, () -> 0, () -> 0, () -> 0, () -> 0).withTimeout(1.3),  //Drive to second ball
             new DriveCommand(drivetrain, () -> 0, () -> 0, () -> 0, () -> 0, () -> 0).withTimeout(1.5),     //Stop Driving
             new IntakeCommand(intake, true).withTimeout(0.1),                                               //Retract Intake
-            new ManualShoot(shooter, -0.4, 1, -0.6),                                                        //Shoot balls into high goal
-            new DriveCommand(drivetrain, () -> -0.5, () -> 0, () -> 0, () -> 0, () -> 0).withTimeout(0.2),  //Drive backwards a little more to make sure it is out of tape
-            new DriveCommand(drivetrain, () -> 0, () -> 0, () -> 0, () -> 0, () -> 0).withTimeout(1),       //Stop Driving backwards
+            // new AutoAim(shooter, drivetrain, true).withTimeout(10),                                                 //Shoot balls into high goal
             new SetT()                                                                                      //Change Auto LED's to tele Mode
         );
     }
