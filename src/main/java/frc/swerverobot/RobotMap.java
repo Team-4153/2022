9a725602bff25181c41984ce25939b7ab6da8d17
Alexr360 <alexr000360@gmail.com>
2022-04-22 10:29:13 -0500

    Removed Left LED Y

--- src/main/java/frc/swerverobot/RobotMap.java
+++ src/main/java/frc/swerverobot/RobotMap.java
@@ -47,7 +47,7 @@ public class RobotMap {
 //LEDS 
     public static final int LEDPWMPort = 5;                                     //[LED Subsystem] Port for the LED's
     public static final boolean RightYLEDS = true;                              //[LED Subsystem] Right Y plugged into the LED strip
-    public static final boolean LeftYLEDS = true;                               //[LED Subsystem] Left Y plugged into the LED strip
+    public static final boolean LeftYLEDS = false;                               //[LED Subsystem] Left Y plugged into the LED strip
     public static final double AutoAimMinDistance = 160;                        //[LED Subsystem] Left Y plugged into the LED strip
     public static final double AutoAimMaxDistance = 190;                        //[LED Subsystem] Left Y plugged into the LED strip
 
